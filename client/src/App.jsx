fd510d5c72495a65ebee42fa17f739ae481fab4f
Sayan Saha <sayansaha555555@gmail.com>
2026-09-08 09:22:30 +0530

    Setup Tailwindcss and assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
-  return (
-    <div className="App">
-      <h1>CareerPilot AI</h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline">
+                Tailwind CSS is working!
+            </h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
